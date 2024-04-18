4dfa81e4c8f2a845f828619fab0ff91a1a6c4701
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-18 15:18:27 +0100

    created filter box component and styled

---
+++ src/components/FilterBox/FilterBox.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Filterbox.scss"
+
+const FilterBox = () => {
+  return (
+	<div className='filter-box'>FilterBox</div>
+  )
+}
+
+export default FilterBox
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import "./NavBar.scss"
 import SearchBox from '../SearchBox/SearchBox'
+import FilterBox from '../FilterBox/FilterBox'
 
 const NavBar = () => {
   return (
-	<div className="nav-bar">
-	NavBar
-	<SearchBox/>
-	<div>This will be ABV % checkbox</div>
-	<div>This will be classic range checkbox</div>
-	<div>This will be acidity checkbox</div>
-	</div>
-	
-	// needs search for name function
-	// needs tickbox function 
-  )
+    <div className="nav-bar">
+      NavBar
+      <SearchBox />
+      <FilterBox />
+      <FilterBox />
+      <FilterBox />
+    </div>
+
+    // needs search for name function
+    // needs tickbox function
+  );
 }
 
 export default NavBar
