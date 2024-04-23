dde769d3c4bec4b1dc8b74799d41fa93786a1b18
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-23 16:07:36 +0100

    added parameters and fixed filtering

--- src/App.tsx
+++ src/App.tsx
@@ -14,54 +14,46 @@ const App = () => {
   const [filterClassicRange, setFilterClassicRange] = useState<boolean>(false);
   const [filterHighAcidity, setFilterHighAcidity] = useState<boolean>(false);
 
-  const getBeers = async (searchNameTerm : string) => {
-    let url =`http://localhost:3333/v2/beers`;
+  const getBeers = async (beerNameSearch : string, AbvFilter : boolean, classicRangeFilter : boolean, HighAcidityFilter : boolean) => {
+    let url = `http://localhost:3333/v2/beers`;
 
-    if (searchNameTerm) {
-      url += `?beer_name=${searchNameTerm}`;
+    // cases where different parameters are entered:
+
+
+    if (beerNameSearch && !AbvFilter && !classicRangeFilter) {         // name search
+      url += `?beer_name=${beerNameSearch}`;
+    } else if (beerNameSearch && AbvFilter && !classicRangeFilter){    // name search and ABV filter
+      url += `?beer_name=${beerNameSearch}&abv_gt=6`;
+    } else if (beerNameSearch && AbvFilter && classicRangeFilter) {    // name search and ABV filter amd classic range filter
+      url += `?beer_name=${beerNameSearch}&abv_gt=6&brewed_before=01-2010`;
+    } else if (beerNameSearch && !AbvFilter && classicRangeFilter) {   // name search and classic range filter
+      url += `?beer_name=${beerNameSearch}&brewed_before=01-2010`;
+    } else if (!beerNameSearch && AbvFilter && classicRangeFilter) {   // abv filter and classic range filter
+      url += `?abv_gt=6&brewed_before=01-2010`;
+    } else if (!beerNameSearch && !AbvFilter && classicRangeFilter) {  // classic range filter 
+      url += `?brewed_before=01-2010`;
+    } else if(!beerNameSearch && AbvFilter && !classicRangeFilter) {   // abv filter
+      url += `?abv_gt=6`
     }
 
     const response = await fetch(url);
     const data: Beer[] = await response.json();
-    console.log(data);
-    setBeers(data);
+    if (HighAcidityFilter) {
+      const filteredHighAcidityBeers = acidityFilter(data)    
+      setBeers(filteredHighAcidityBeers)
+    } else {
+      setBeers(data);
+    }
   };
-
-  useEffect(() => {
-    // apply filters whenever filter criteria or beer data changes
-    setFilteredBeers(applyFilters(beers));
-  }, [beers, filterAbv, filterClassicRange, filterHighAcidity]);
-
-  // note: [beers, filterAbv, filterClassicRange, filterHighAcidity] are the dependencies/options of the useEffect hook.
 
   useEffect(() => {
     // run the code we want to run when the page first loads
-    getBeers(searchNameTerm);
-  }, [searchNameTerm]);
+    getBeers(searchNameTerm, filterAbv, filterClassicRange, filterHighAcidity);
+  }, [searchNameTerm, filterAbv, filterClassicRange, filterHighAcidity]);
 
-  const applyFilters = (beers: Beer[]): Beer[] => {
-    return beers.filter((beer) => {
-      // include beers with abv > 6 if filterAbv is true
-      if (filterAbv && beer.abv >= 6) {
-        return true;
-      }
-
-      // include beers brewed before 2010 if filterClassicRange is true
-      if (filterClassicRange) {
-        const yearBrewed = parseInt(beer.first_brewed.split("/")[1]);
-        if (yearBrewed <= 2010) {
-          return true;
-        }
-      }
-
-      if (filterHighAcidity && beer.ph <= 4) {
-        return true;
-      }
-
-      // include all other beers
-      return false;
-    });
-  };
+const acidityFilter = (beers: Beer[]): Beer[] => {
+  return beers.filter((beer) => beer.ph <= 4);
+};
 
   return (
     <div className="app">
