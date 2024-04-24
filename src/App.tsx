2acb88f42fc177704b07a1c131e28c2e7252ed63
Byasar3 <bernayasar@hotmail.co.uk>
2024-04-24 12:59:57 +0100

    foundations for pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,44 +3,61 @@ import "./App.scss";
 import Main from "./components/Main/Main";
 import NavBar from "./components/NavBar/NavBar";
 import Beer from "./types/Beer";
+import Pagination from "./components/Pagination/Pagination";
 
 const App = () => {
   const [beers, setBeers] = useState<Beer[]>([]);
-  const [filteredBeers, setFilteredBeers] = useState<Beer[]>([]);
 
   // search stuff
   const [searchNameTerm, setSearchNameTerm] = useState<string>("");
   const [filterAbv, setFilterAbv] = useState<boolean>(false);
   const [filterClassicRange, setFilterClassicRange] = useState<boolean>(false);
   const [filterHighAcidity, setFilterHighAcidity] = useState<boolean>(false);
-
-  const getBeers = async (beerNameSearch : string, AbvFilter : boolean, classicRangeFilter : boolean, HighAcidityFilter : boolean) => {
-    let url = `http://localhost:3333/v2/beers`;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  
+  const getBeers = async (
+    beerNameSearch: string,
+    AbvFilter: boolean,
+    classicRangeFilter: boolean,
+    HighAcidityFilter: boolean,
+    pageNumber: number
+  ) => {
+    let url = `http://localhost:3333/v2/beers?page=${pageNumber}&per_page=40`;
+    
+    // needs to be a prop that changes the page number -> comes from main -> need buttons or pagination component?
+    // need a function somewhere that will increment setCurrentPage when buttons are pressed
+    // need a usestate for this too, to manage the state
 
     // cases where different parameters are entered:
 
-
-    if (beerNameSearch && !AbvFilter && !classicRangeFilter) {         // name search
-      url += `?beer_name=${beerNameSearch}`;
-    } else if (beerNameSearch && AbvFilter && !classicRangeFilter){    // name search and ABV filter
-      url += `?beer_name=${beerNameSearch}&abv_gt=6`;
-    } else if (beerNameSearch && AbvFilter && classicRangeFilter) {    // name search and ABV filter amd classic range filter
-      url += `?beer_name=${beerNameSearch}&abv_gt=6&brewed_before=01-2010`;
-    } else if (beerNameSearch && !AbvFilter && classicRangeFilter) {   // name search and classic range filter
-      url += `?beer_name=${beerNameSearch}&brewed_before=01-2010`;
-    } else if (!beerNameSearch && AbvFilter && classicRangeFilter) {   // abv filter and classic range filter
-      url += `?abv_gt=6&brewed_before=01-2010`;
-    } else if (!beerNameSearch && !AbvFilter && classicRangeFilter) {  // classic range filter 
-      url += `?brewed_before=01-2010`;
-    } else if(!beerNameSearch && AbvFilter && !classicRangeFilter) {   // abv filter
-      url += `?abv_gt=6`
+    if (beerNameSearch && !AbvFilter && !classicRangeFilter) {
+      // name search
+      url += `&beer_name=${beerNameSearch}`;
+    } else if (beerNameSearch && AbvFilter && !classicRangeFilter) {
+      // name search and ABV filter
+      url += `&beer_name=${beerNameSearch}&abv_gt=6`;
+    } else if (beerNameSearch && AbvFilter && classicRangeFilter) {
+      // name search and ABV filter amd classic range filter
+      url += `&beer_name=${beerNameSearch}&abv_gt=6&brewed_before=01-2010`;
+    } else if (beerNameSearch && !AbvFilter && classicRangeFilter) {
+      // name search and classic range filter
+      url += `&beer_name=${beerNameSearch}&brewed_before=01-2010`;
+    } else if (!beerNameSearch && AbvFilter && classicRangeFilter) {
+      // abv filter and classic range filter
+      url += `&abv_gt=6&brewed_before=01-2010`;
+    } else if (!beerNameSearch && !AbvFilter && classicRangeFilter) {
+      // classic range filter
+      url += `&brewed_before=01-2010`;
+    } else if (!beerNameSearch && AbvFilter && !classicRangeFilter) {
+      // abv filter
+      url += `&abv_gt=6`;
     }
 
     const response = await fetch(url);
     const data: Beer[] = await response.json();
     if (HighAcidityFilter) {
-      const filteredHighAcidityBeers = acidityFilter(data)    
-      setBeers(filteredHighAcidityBeers)
+      const filteredHighAcidityBeers = acidityFilter(data);
+      setBeers(filteredHighAcidityBeers);
     } else {
       setBeers(data);
     }
@@ -48,12 +65,24 @@ const App = () => {
 
   useEffect(() => {
     // run the code we want to run when the page first loads
-    getBeers(searchNameTerm, filterAbv, filterClassicRange, filterHighAcidity);
-  }, [searchNameTerm, filterAbv, filterClassicRange, filterHighAcidity]);
+    getBeers(
+      searchNameTerm,
+      filterAbv,
+      filterClassicRange,
+      filterHighAcidity,
+      currentPage
+    );
+  }, [
+    searchNameTerm,
+    filterAbv,
+    filterClassicRange,
+    filterHighAcidity,
+    currentPage,
+  ]);
 
-const acidityFilter = (beers: Beer[]): Beer[] => {
-  return beers.filter((beer) => beer.ph <= 4);
-};
+  const acidityFilter = (beers: Beer[]): Beer[] => {
+    return beers.filter((beer) => beer.ph <= 4);
+  };
 
   return (
     <div className="app">
@@ -64,7 +93,8 @@ const acidityFilter = (beers: Beer[]): Beer[] => {
         setFilterClassicRange={setFilterClassicRange}
         setFilterHighAcidity={setFilterHighAcidity}
       />
-      <Main filteredBeers={filteredBeers.length ? filteredBeers : beers} />
+      <Main filteredBeers={beers} />
+      <Pagination/>
     </div>
   );
 };
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,14 @@
+
+const Pagination = () => {
+
+
+  return (
+    <div className="pagination">
+      <button className="pagination-prev"> Previous Page </button>
+      <button className="pagination-current"> </button>
+      <button className="pagination-next"> Next Page </button>
+    </div>
+  );
+};
+
+export default Pagination;
